fc5588b7ce9c3ac86cda9897a6f8b81a72dd8a04
caquijano <carlosquijano98@gmail.com>
2021-04-21 19:53:09 -0500

    heroku host

--- src/config.ts
+++ src/config.ts
@@ -2,6 +2,6 @@ export default{
     MONGO_DATABASE: 'green',
     MONGO_USER: 'admin',
     MONGO_PASSWORD: 'admin',
-    MONGO_HOST: 'localhost',
+    MONGO_HOST: "https://damp-dusk-50080.herokuapp.com",
     SECRET: 'ikhodi'
 }
