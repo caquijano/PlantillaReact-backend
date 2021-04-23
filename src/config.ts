f8ffb3bdc5ab2f36759179ee181c29e6752340e1
caquijano <carlosquijano98@gmail.com>
2021-04-23 14:31:41 -0500

    cambios base de datos

--- src/config.ts
+++ src/config.ts
@@ -2,6 +2,6 @@ export default{
     MONGO_DATABASE: 'green',
     MONGO_USER: 'admin',
     MONGO_PASSWORD: 'admin',
-    MONGO_HOST: 'https://damp-dusk-50080.herokuapp.com',
+    MONGO_HOST: 'admin:admin@ikhodi.kmdva.mongodb.net',
     SECRET: 'ikhodi'
 }
